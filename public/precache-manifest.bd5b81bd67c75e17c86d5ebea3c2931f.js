bac166d511103e3c540da0611cc3b9a3ee894794
Coloring <1561999073@qq.com>
2020-03-30 00:39:09 +0800

    第一次提交

---
+++ public/precache-manifest.bd5b81bd67c75e17c86d5ebea3c2931f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbb3d81c429d8eb744d3b8f2270316b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34ee49e9c898bd61156d",
+    "url": "/static/css/2.a20e6b46.chunk.css"
+  },
+  {
+    "revision": "42bde04b3a160af7bcec",
+    "url": "/static/css/main.c9177f76.chunk.css"
+  },
+  {
+    "revision": "34ee49e9c898bd61156d",
+    "url": "/static/js/2.0dd41a64.chunk.js"
+  },
+  {
+    "revision": "42bde04b3a160af7bcec",
+    "url": "/static/js/main.f6e098d8.chunk.js"
+  },
+  {
+    "revision": "5fb4730b3ce173b83c4b",
+    "url": "/static/js/runtime-main.c3cb7645.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/static/media/logo.ba1f87ec.png"
+  }
+]);
